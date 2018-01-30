a0b934b69b139654f65b97d180314c314ed0324d
nsmith677 <nicole.smith677@gmail.com>
2018-01-30 16:34:43 -0500

    Project Setup from Test2 Repo

---
+++ TEST2-master/CIS420TeamA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CIS420TeamA.Startup))]
+namespace CIS420TeamA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
