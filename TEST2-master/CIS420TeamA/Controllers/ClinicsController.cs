09779a1bce189278c51b98fcda8bc7607f8d1e4c
Gopal Parmar <GP@DESKTOP-IDCBSNK>
2018-02-28 19:37:18 -0500

    partner clinics page

---
+++ TEST2-master/CIS420TeamA/Controllers/ClinicsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CIS420TeamA.Controllers
+{
+    public class ClinicsController : Controller
+    {
+        // GET: Clinics
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
